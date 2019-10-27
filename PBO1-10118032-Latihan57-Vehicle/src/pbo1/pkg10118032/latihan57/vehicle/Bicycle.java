f9cdc90b532c60eec7939fd711a7a92a3c9bbfc2
Guntur Maulana Ibrahim <55784684+gunturmaulanaii@users.noreply.github.com>
2019-10-27 19:00:28 +0700

    Add files via upload

---
+++ PBO1-10118032-Latihan57-Vehicle/src/pbo1/pkg10118032/latihan57/vehicle/Bicycle.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118032.latihan57.vehicle;
+
+/**
+ *
+ * @author Guntur Maulana Ibrahim
+ * NAMA  : Guntur Maulana Ibrahim
+ * KELAS : IF 1
+ * NIM   : 10118032
+ */
+public class Bicycle extends Vehicle{
+    
+    private int myGearCount;
+    
+    public Bicycle (){
+        System.out.println("Bicycle");
+    }
+
+    public int getMyGearCount() {
+        return myGearCount;
+    }
+
+    public void setMyGearCount(int myGearCount) {
+        this.myGearCount = myGearCount;
+    }
+    
+    
+}
